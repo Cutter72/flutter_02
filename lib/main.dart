c629618ff92429d0e6c52e690a3fa29d701a93e9
Paweł Drelich <drelich_pawel@o2.pl>
2022-10-28 22:48:50 +0200

    Init locale for date format implemented. Date look updated.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_02/transaction.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:intl/intl_standalone.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +12,10 @@ void main() {
 /// @author Paweł Drelich <drelich_pawel@o2.pl>
 ///
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  MyApp() {
+    initLocale();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,6 +25,11 @@ class MyApp extends StatelessWidget {
       ),
       home: MyHomePage(),
     );
+  }
+
+  void initLocale() {
+    Future.wait([findSystemLocale()]);
+    initializeDateFormatting(Intl.systemLocale);
   }
 }
 
@@ -53,7 +64,7 @@ class MyHomePage extends StatelessWidget {
               return Card(
                 elevation: 5,
                 child: Row(
-                  children: [
+                  children: <Widget>[
                     Container(
                       margin: EdgeInsets.all(8),
                       padding: EdgeInsets.symmetric(vertical: 6, horizontal: 8),
@@ -66,32 +77,45 @@ class MyHomePage extends StatelessWidget {
                         "\$${tx.amount}",
                       ),
                     ),
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 2, horizontal: 4),
-                          child: Text(
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(
-                              color: Colors.blueAccent,
-                              fontSize: 14,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 1,
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 2, horizontal: 4),
+                            child: Text(
+                              textAlign: TextAlign.justify,
+                              style: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold,
+                                letterSpacing: 1,
+                              ),
+                              tx.title.toUpperCase(),
                             ),
-                            tx.title.toUpperCase(),
                           ),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 2, horizontal: 4),
-                          child: Text(
-                            textAlign: TextAlign.justify,
-                            style: TextStyle(fontSize: 14, color: Colors.grey),
-                            "Date: ${tx.date.toLocal().toString()}",
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 2, horizontal: 4),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              // crossAxisAlignment: CrossAxisAlignment.stretch,
+                              children: [
+                                Text(
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                                  "${DateFormat.Hms(Intl.systemLocale).format(tx.date)}",
+                                ),
+                                Text(
+                                  textAlign: TextAlign.justify,
+                                  style: TextStyle(fontSize: 14, color: Colors.grey),
+                                  "${DateFormat.yMMMd(Intl.systemLocale).format(tx.date)}",
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ],
                 ),
