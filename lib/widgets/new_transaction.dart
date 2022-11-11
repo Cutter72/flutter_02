281a060687b7db9af41f1f0c1cbc899b684def38
Paweł Drelich <drelich_pawel@o2.pl>
2022-11-11 21:54:58 +0100

    Day 6 Date picker button added & TextFields decoration updated.

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_02/models/transaction.dart';
+import 'package:intl/intl.dart';
 
 ///
 /// @author Paweł Drelich <drelich_pawel@o2.pl>
@@ -28,19 +29,40 @@ class _NewTransactionState extends State<NewTransaction> {
         margin: EdgeInsets.all(8),
         width: double.infinity,
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.end,
           children: [
             TextField(
-              decoration: InputDecoration(labelText: "Title"),
+              decoration: InputDecoration(labelText: "Title", border: OutlineInputBorder()),
               controller: titleController,
             ),
             TextField(
               keyboardType: TextInputType.numberWithOptions(decimal: true),
-              decoration: InputDecoration(labelText: "Amount \$.\$\$"),
+              decoration: InputDecoration(labelText: "Amount \$.\$\$", border: OutlineInputBorder()),
               controller: amountController,
               onChanged: (input) => {print("onChanged: input=${amountController.text}")},
               onEditingComplete: () => {print("onEditingComplete")},
               onSubmitted: (input) => {submitTransaction()},
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                    border: Border.all(color: Theme.of(context).disabledColor)),
+                child: TextButton(
+                  child: Text("Date: ${DateFormat.yMd(Intl.systemLocale).format(DateTime.now())}",
+                      textAlign: TextAlign.justify),
+                  onPressed: () {
+                    final today = DateTime.now();
+                    showDatePicker(
+                            context: context, initialDate: today, firstDate: DateTime(today.year - 1), lastDate: today)
+                        .then((pickedDate) {
+                      if (pickedDate != null) {}
+                    });
+                  },
+                ),
+              ),
             ),
             ElevatedButton(
               child: Text("Add transaction"),
@@ -56,7 +78,7 @@ class _NewTransactionState extends State<NewTransaction> {
     // if (titleController.text.isEmpty || amountController.text.isEmpty) {
     //   return;
     // } else {
-      widget._addTransaction(Transaction(title: titleController.text, amount: double.tryParse(amountController.text)));
+    widget._addTransaction(Transaction(title: titleController.text, amount: double.tryParse(amountController.text)));
     // }
     Navigator.of(context).pop();
   }
